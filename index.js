d1f098a10ada981156a19f2e5276457b173a3870
shanmugapriya <shanmugapriya.rajendran@gmail.com>
2023-09-09 03:39:34 +0800

    index.jsl

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 'use strict';
 
 const express = require('express');
+app.use(express.static('src'));
 
 // Constants
 const PORT = 8080;
@@ -18,16 +19,21 @@ const ENV = 'DEV';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from Node 4!';
-  res.send(msg);
+  //res.statusCode = 200;
+  //const msg = 'Hello from Node 4!';
+  //res.send(msg);
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(msg);
+//app.get('/test', (req, res) => {
+  //res.statusCode = 200;
+  //const msg = 'Hello from /test Node!';
+  //res.send(msg);
+
+  const server = app.listen(8080, () => {
+    console.log('Server started on http://localhost:8080');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+module.exports = server; // Export server instance for testing
